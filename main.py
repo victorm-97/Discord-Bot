f0616bdd5f23fe21e08fd1afc40e5257a08c2a40
victorm-97 <62194033+victorm-97@users.noreply.github.com>
2025-06-08 20:08:11 -0700

    Add files via upload

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import logging.handlers
 import discord
 from discord.ext import commands
 import logging
@@ -14,8 +15,73 @@ intents.members = True
 
 bot = commands.Bot(command_prefix='!', intents=intents)
 
+
+based_role = "Femboy"
+
 @bot.event
 async def on_ready():
-    print("Were ready to go in, {bot.user.name}")
+    print(f"We're ready to go in {bot.user.name}")
 
-bot.run(token, log_handler=handler, log_level=logging.DEBUG)
+@bot.event
+async def on_member_join(member):
+    await member.send(f"Welcome to the server {member.name}")   
+
+@bot.event
+async def on_message(message):
+    if message.author == bot.user:
+        return
+    if "shit" in message.content.lower():
+        await message.delete()
+        await message.channel.send(f"{message.author.mention} don't use that word!")
+    
+    await bot.process_commands(message)
+
+# !hello
+@bot.command()
+async def hello(ctx):
+    await ctx.send(f"Hello {ctx.author.mention}")
+
+@bot.command()
+async def assign(ctx):
+    role = await discord.utils.get_role(ctx.guild.roles, name="Femboy")
+    if role:
+        await ctx.author.add_roles(role)
+        await ctx.send(f"{ctx.author.mention} is now assigned to {based_role}")
+    else:
+        await ctx.send("Role doesn't exist")
+
+@bot.command()
+async def remove(ctx):
+    role = discord.utils.get(ctx.guild.roles, name="Femboy")
+    if role:
+        await ctx.author.remove_roles(role)
+        await ctx.send(f"{ctx.author.mention} has had the {based_role} removed")
+    else:
+        await ctx.send("Role doesn't exist")
+
+@bot.command()
+async def dm(ctx, *, msg):
+    await ctx.author.send(f"You said {msg}")
+
+@bot.command
+async def reply(ctx):
+    await ctx.reply("This is a reply to your message!")
+
+@bot.command()
+async def poll(ctx, *, question):
+    embed = discord.Embed(title="New Poll", description=question)
+    poll_message= await ctx.send(embed=embed)
+    await poll_message.add_reaction("😭")
+    await poll_message.add_reaction("👍")
+
+@bot.command
+@commands.has_role(based_role)
+async def cool(ctx):
+    await ctx.send("Welcome to the club!")
+
+@bot.event
+async def cool_error(ctx, error):
+    if isinstance(error, commands.MissingRole):
+        await ctx.send("You do not have permession to do that!")
+
+bot.run(token)
